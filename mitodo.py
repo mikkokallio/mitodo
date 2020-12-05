b60e0436c6e8ece6c51581cf6a90a9b102d4fc1c
Mikko Kallio <mikko.kallio@utopiaanalytics.com>
2020-12-05 16:48:15 +0200

    add tasklist and tests

--- mitodo.py
+++ mitodo.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class Task:
     """An individual task item to put on a to-do list"""
 
@@ -28,6 +31,78 @@ class Task:
         """Get task text"""
         return self.__text
 
+
+class TaskList:
+    """A to-do list"""
+
+    def __init__(self, min_length=2):
+        """Create list of tasks with optional min length for task names"""
+        self.__tasks = []
+        self.__min_length = min_length
+        self.id_generator = (i for i in itertools.count(1))
+
+    def __str__(self):
+        """Print task list"""
+        return str([str(task) for task in (self.list_tasks())])
+
+    def __len__(self):
+        """Return total number of tasks"""
+        return len(self.__tasks)
+
+    def list_tasks(self, order_abc=False, filter_done=False) -> list:
+        """List tasks ordered by id (default) or alphabetically, and optional filtering of completed tasks"""
+        tasks = self.__tasks[:]
+        if filter_done:
+            tasks = [task for task in self.__tasks if not task.is_done()]
+        if order_abc:
+            tasks = sorted(self.__tasks, key=lambda task: task.get_text())
+        return tasks
+
+    def add_task(self, text: str, done=False) -> Task:
+        """Add a new task to the task list, with an incremental id, and return task"""
+        if len(text) >= self.__min_length:
+            task = Task(text, str(next(self.id_generator)), done)
+            self.__tasks.append(task)
+            return task
+        else:
+            raise ValueError(
+                f'text must be at least {self.__min_length} characters')
+
+    def complete_task(self, id: int) -> Task:
+        """Search task by id; if found, mark as completed and return the task"""
+        task = next(
+            (task for task in self.__tasks if task.get_id() == id), None)
+        if task is not None:
+            if task.is_done():
+                raise ValueError('task already completed')
+            else:
+                task.set_done()
+                return task
+        else:
+            raise ValueError('id not found')
+
+    def remove_task(self, id: int) -> Task:
+        """Search task by id; if found remove and return the task"""
+        task = next(
+            (task for task in self.__tasks if task.get_id() == id), None)
+        if task is not None:
+            self.__tasks = [
+                task for task in self.__tasks if task.get_id() != id]
+            return task
+        else:
+            raise ValueError('id not found')
+
+    def remove_completed_tasks(self) -> int:
+        """Remove all tasks that have been marked as completed"""
+        n = len(self.__tasks)
+        self.__tasks = [task for task in self.__tasks if not task.is_done()]
+        return n - len(self.__tasks)
+
+
 if __name__ == "__main__":
-    task = Task('Create todo app', 1)
-    print(task)
+    tasklist = TaskList()
+    tasklist.add_task('Add tasklist to todo app')
+    tasklist.add_task('Add second task')
+    n = tasklist.add_task('Yoyoyo')
+    print(tasklist)
+    print(n.get_id())
--- test.py
+++ test.py
@@ -4,12 +4,55 @@ from mitodo import *
 
 class TestTask(unittest.TestCase):
 
-    def test_add_task(self):
-        """Test create and complete task"""
+    def test_task(self):
+        """Test creating and completing a task"""
         task = Task('Create test task', 1)
         self.assertEqual(str(task), '[ ]  1: Create test task')
         task.set_done()
         self.assertEqual(str(task), '[X]  1: Create test task')
+
+
+class TestTaskList(unittest.TestCase):
+
+    def test_add_task(self):
+        """Test tasks get added"""
+        tasklist = TaskList()
+        tasklist.add_task('Create test task')
+        self.assertEqual(len(tasklist), 1)
+
+    def test_too_short(self):
+        """Test too short task raises error"""
+        with self.assertRaises(ValueError) as context:
+            tasklist = TaskList(min_length=4)
+            tasklist.add_task('Yo!')
+        self.assertTrue(
+            'text must be at least 4 characters' in str(context.exception))
+
+    def test_remove_completed(self):
+        """Test that only completed are removed"""
+        tasklist = TaskList()
+        t1 = tasklist.add_task('Foo').get_id()
+        t2 = tasklist.add_task('Bar').get_id()
+        t3 = tasklist.add_task('Foo2').get_id()
+        t4 = tasklist.add_task('Bar2').get_id()
+        t5 = tasklist.add_task('Foo3').get_id()
+        tasklist.complete_task(t1)
+        tasklist.complete_task(t2)
+        tasklist.remove_completed_tasks()
+        self.assertEqual(len(tasklist), 3)
+        self.assertEqual(tasklist.list_tasks()[0].get_text(), 'Foo2')
+
+    def test_remove_one(self):
+        """Test that any task can be removed"""
+        tasklist = TaskList()
+        t1 = tasklist.add_task('Foo').get_id()
+        t2 = tasklist.add_task('Bar').get_id()
+        t3 = tasklist.add_task('Foo2').get_id()
+        t4 = tasklist.add_task('Bar2').get_id()
+        t5 = tasklist.add_task('Foo3').get_id()
+        removed = tasklist.remove_task(t3)
+        self.assertEqual(removed.get_text(), 'Foo2')
+        self.assertEqual(len(tasklist), 4)
 
 
 if __name__ == "__main__":
