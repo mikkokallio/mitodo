3b4f3c072613ce81efa1093083311d7df60a419f
Mikko Kallio <mikko.kallio@utopiaanalytics.com>
2020-12-05 16:03:29 +0200

    add basic task test

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-# TODO: Add tests
+import unittest
+from mitodo import *
+
+
+class TestTask(unittest.TestCase):
+
+    def test_add_task(self):
+        """Test create and complete task"""
+        task = Task('Create test task', 1)
+        self.assertEqual(str(task), '[ ]  1: Create test task')
+        task.set_done()
+        self.assertEqual(str(task), '[X]  1: Create test task')
+
+
+if __name__ == "__main__":
+    unittest.main()
